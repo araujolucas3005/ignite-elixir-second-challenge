4a2e1f9eadbbe8058087bc4adeec94959689e2f6
araujolucas3005 <araujolucas3005@gmail.com>
2022-03-16 22:06:21 -0300

    tests done

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,13 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "should return the quantity of odd numbers from a list of strings" do
+      list = ["ola", "mundo", "1", "2", "3", "100", "101"]
+
+      result = ListFilter.call(list)
+
+      assert response = 3
+    end
   end
 end
